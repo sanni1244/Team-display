59f9100d8fa9727f2cc1ce076dbce261229a0232
Opeyemi <opeyemi.sanni180591052@st.lasu.edu.ng>
2023-07-22 20:32:57 +0100

    final

--- src/components/editemployee.js
+++ src/components/editemployee.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Button, Modal, Form } from "react-bootstrap";
 
-function Edit({ member }) {
+function Edit({ member, update }) {
     const [showModal, setShowModal] = useState(false);
     const [name, setName] = useState(member.name);
     const [role, setRole] = useState(member.role);
@@ -9,16 +9,20 @@ function Edit({ member }) {
     const handleCloseModal = () => setShowModal(false);
     const handleShowModal = () => setShowModal(true);
 
-    
     const handleSubmit = (e) => {
         e.preventDefault();
-      
+        // Call the update function passed from the TeamSection component
+        update(member.id, name, role);
         handleCloseModal();
     };
 
     return (
         <div className="flex justify-center items-center">
-            <button id="submitform" className="bg-gradient-to-r from-blue-500 to-purple-600 hover:from-blue-600 hover:to-purple-700 text-white font-semibold py-3 px-6 rounded-full shadow-lg animate-pulse" variant="primary" onClick={handleShowModal}>
+            <button
+                className="bg-gradient-to-r from-blue-500 to-purple-600 hover:from-blue-600 hover:to-purple-700 text-white font-semibold py-3 px-6 rounded-full shadow-lg animate-pulse"
+                variant="primary"
+                onClick={handleShowModal}
+            >
                 Open Modal
             </button>
             <Modal show={showModal} onHide={handleCloseModal}>
@@ -26,21 +30,30 @@ function Edit({ member }) {
                     <Modal.Title>Edit User</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <form onSubmit={(e) => {
-                        e.preventDefault();
-                        console.log("Hello")
-                    }}>
-                    <Form.Group  className="mb-3" id="">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter your Name" onChange = {(e) => {setName(e.target.value)}} value={name} id="name" />
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Role</Form.Label>
-                        <Form.Control type="text" placeholder="Enter your Role" onChange = {(e) => {setRole(e.target.value)}} value={role} id="role" />
-                    </Form.Group>
+                    <form>
+                        <Form.Group className="mb-3" id="">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter your Name"
+                                onChange={(e) => { setName(e.target.value) }}
+                                value={name}
+                                id="name"
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Role</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter your Role"
+                                onChange={(e) => { setRole(e.target.value) }}
+                                value={role}
+                                id="role"
+                            />
+                        </Form.Group>
                     </form>
                 </Modal.Body>
-                <Modal.Footer> 
+                <Modal.Footer>
                     <button
                         className="px-4 py-2 bg-pink-500 text-white rounded-md mr-2"
                         onClick={handleCloseModal}
@@ -48,7 +61,6 @@ function Edit({ member }) {
                         Close
                     </button>
                     <button
-                        form="submitform"
                         type="submit"
                         className="px-4 py-2 bg-indigo-500 text-white rounded-md"
                         onClick={handleSubmit}
--- src/components/employee.js
+++ src/components/employee.js
@@ -6,37 +6,49 @@ import Editemployee from './editemployee';
 const TeamSection = () => {
     const [teamMember, setTeamMembers] = useState([
         {
+            id: 1,
             name: "Diana Zale",
             role: "CEO",
             image: "https://media.istockphoto.com/id/997661938/photo/close-up-portrait-of-smiling-pretty-girl-in-yellow-sweater-isolated-on-gray-background.jpg?s=612x612&w=0&k=20&c=ziL4OdLh1I5VlqRwoPJwrCid0gYjjGfnmtUUhalEQMk=",
         },
         {
+            id: 2,
             name: "Jane Smith",
             role: "Manager",
             image: "https://img.freepik.com/free-photo/portrait-dark-skinned-cheerful-woman-with-curly-hair-touches-chin-gently-laughs-happily-enjoys-day-off-feels-happy-enthusiastic-hears-something-positive-wears-casual-blue-turtleneck_273609-43443.jpg",
         },
         {
+            id: 3,
             name: "Adam Copeland",
             role: "Administrator",
             image: "https://media.istockphoto.com/id/1095898284/photo/portrait-of-thinking-young-man-looking-away.jpg?s=612x612&w=0&k=20&c=5Gq1OY470MHxs8c1J-spmJYg1Iwp4ABEzsaG2tUmG1Q=",
         },
         {
+            id: 4,
             name: "Mathew Jacob",
             role: "Chef",
             image: "https://thumbs.dreamstime.com/b/young-man-smiling-beard-wearing-green-shirt-isolated-white-background-92802989.jpg",
         },
         {
+            id: 5,
             name: "Pamela Rose",
             role: "Operations Manager",
             image: "https://wrestlingnews.co/.image/t_share/MTkwMDM0MTMyNzMxNTY5MjQ1/bayley.jpg",
         },
     ]);
 
-    function update(id, newName, newRole){
-
-        console.log("Updated");
-
+    function update(id, newName, newRole) {
+        // Find the team member with the provided id and update its name and role
+        const updatedMembers = teamMember.map((member) =>
+            member.id === id ? { ...member, name: newName, role: newRole } : member
+        );
+    
+        setTeamMembers(updatedMembers);
+    
+        // Save the updated data in local storage
+        localStorage.setItem('teamMembers', JSON.stringify(updatedMembers));
     }
+
 
     return (
         <section className="py-12">
@@ -54,7 +66,7 @@ const TeamSection = () => {
                                 <div className="p-6 rounded-lg text-center">
                                     <h3 className="text-2xl font-bold text-blue-600 mb-2">{member.name}</h3>
                                     <p className="text-gray-500 text-lg animate-bounce">{member.role}</p>
-                            <Editemployee member={member} update={member.Editemployee}/> 
+                                    <Editemployee member={member} update={update} />
                                 </div>
                             </div>
                         </div>
